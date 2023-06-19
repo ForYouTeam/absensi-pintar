c6ce5b90561278e7898ba620c4aeb07f005e7248
IPKIN9 <irwandirini@gmail.com>
2023-06-19 17:14:50 +0800

    Mapel

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Interfaces\JurusanInterface;
+use App\Interfaces\MapelInterface;
 use App\Repositories\JurusanRepository;
+use App\Repositories\MapelRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -26,5 +28,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(JurusanInterface::class, JurusanRepository::class);
+        $this->app->bind(MapelInterface::class, MapelRepository::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\MapelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/jurusan')->controller(JurusanController::class)->group(function () {
-	Route::get    ('/'     , 'getPayloadData'    );
-	Route::get    ('/{id}' , 'getPayloadDataId'  );
-	Route::post   ('/'     , 'upsertPayloadData' );
-	Route::get ('/{id}' , 'deletePayloadData' );
+	Route::get  ('/'     , 'getPayloadData'    );
+	Route::get  ('/{id}' , 'getPayloadDataId'  );
+	Route::post ('/'     , 'upsertPayloadData' );
+	Route::delete  ('/{id}' , 'deletePayloadData' );
+});
+
+Route::prefix('v1/mapel')->controller(MapelController::class)->group(function () {
+	Route::get  ('/'     , 'getPayloadData'    );
+	Route::get  ('/{id}' , 'getPayloadDataId'  );
+	Route::post ('/'     , 'upsertPayloadData' );
+	Route::delete  ('/{id}' , 'deletePayloadData' );
 });
