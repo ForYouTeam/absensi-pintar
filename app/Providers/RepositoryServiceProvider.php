10e22ded40916992e15a51f36e4aa6ecb5061e63
IPKIN9 <irwandirini@gmail.com>
2023-06-19 19:02:32 +0800

    Siswa done

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,11 +7,13 @@ use App\Interfaces\JabatanInterface;
 use App\Interfaces\JurusanInterface;
 use App\Interfaces\KelasInterface;
 use App\Interfaces\MapelInterface;
+use App\Interfaces\SiswaInterface;
 use App\Repositories\GuruRepository;
 use App\Repositories\JabatanRepository;
 use App\Repositories\JurusanRepository;
 use App\Repositories\KelasRepository;
 use App\Repositories\MapelRepository;
+use App\Repositories\SiswaRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -33,10 +35,11 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind(JurusanInterface::class, JurusanRepository::class);
-        $this->app->bind(MapelInterface  ::class, MapelRepository  ::class);
-        $this->app->bind(JabatanInterface::class, JabatanRepository::class);
-        $this->app->bind(KelasInterface  ::class, KelasRepository  ::class);
-        $this->app->bind(GuruInterface   ::class, GuruRepository   ::class);
+        $this->app->bind(JurusanInterface ::class, JurusanRepository ::class);
+        $this->app->bind(MapelInterface   ::class, MapelRepository   ::class);
+        $this->app->bind(JabatanInterface ::class, JabatanRepository ::class);
+        $this->app->bind(KelasInterface   ::class, KelasRepository   ::class);
+        $this->app->bind(GuruInterface    ::class, GuruRepository    ::class);
+        $this->app->bind(SiswaInterface   ::class, SiswaRepository   ::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\JabatanController;
 use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\MapelController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -37,6 +38,13 @@ Route::prefix('v1/kelas')->controller(KelasController::class)->group(function ()
 });
 
 Route::prefix('v1/guru')->controller(GuruController::class)->group(function () {
+	Route::get  ('/'     , 'getPayloadData'    );
+	Route::get  ('/{id}' , 'getPayloadDataId'  );
+	Route::post ('/'     , 'upsertPayloadData' );
+	Route::delete  ('/{id}' , 'deletePayloadData' );
+});
+
+Route::prefix('v1/siswa')->controller(SiswaController::class)->group(function () {
 	Route::get  ('/'     , 'getPayloadData'    );
 	Route::get  ('/{id}' , 'getPayloadDataId'  );
 	Route::post ('/'     , 'upsertPayloadData' );
